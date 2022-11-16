cf44b497fca971c9434d9f41d43197ba12f06ca3
sycDev <sinyi.20028@gmail.com>
2022-11-16 10:59:43 +0800

    Add and display data

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HousingLocation } from "../housing-location";
 
 @Component({
   selector: 'app-housing-list',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HousingListComponent implements OnInit {
 
+  @Input() locationList: HousingLocation[] = [];
+  
   constructor() { }
 
   ngOnInit(): void {
